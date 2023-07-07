88d618e651987c92c456612af128ca8e162ed972
Phong Lưu <ppluu1692@gmail.com>
2023-07-07 17:35:20 +0700

    make web interface

---
+++ FE/src.js
@@ -0,0 +1,39 @@
+var resultTxt = "";
+
+function type() {
+    const currentText = document.querySelector("#auto-type").textContent;
+
+    if (currentText.length < resultTxt.length) {
+        document.querySelector("#auto-type").textContent += resultTxt[currentText.length];
+        setTimeout(type, 80);
+    }
+}
+
+async function onSubmit() {
+    const currentText = document.querySelector("#auto-type");
+    currentText.innerHTML = '';
+    text = inputTxt.value;
+
+    let myObject = await fetch(`http://localhost:5000/predict?link="${text}"`);
+    let response = await myObject.text();
+
+    if (response === 'YES') {
+        resultTxt = "Your tweet is about REAL disasters."
+    } else if (response == 'NO') {
+        resultTxt = "Your tweet is not about disasters."
+    } else {
+        resultTxt = "Something went wrong :(("
+    }
+    type();
+}
+const inputTxt = document.querySelector(".inputTxt")
+const submitBtn = document.querySelector(".btnSearch")
+
+submitBtn.addEventListener('click', onSubmit)
+
+inputTxt.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        onSubmit();
+        inputTxt.select();
+    }
+});
