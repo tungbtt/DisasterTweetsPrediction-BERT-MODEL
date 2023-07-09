99b294c24ae0f27c113367d9704bf0b035cbdba3
Phong Lưu <ppluu1692@gmail.com>
2023-07-09 20:24:26 +0700

    add some comments

--- FE/src.js
+++ FE/src.js
@@ -6,7 +6,7 @@ const tweetEle = document.querySelector("#tweet");
 const resultEle = document.querySelector("#result");
 const resultLoader = document.querySelector("#result-loading");
 
-
+// show result with typing effect
 function type() {
     const currentText = resultEle.textContent;
     if (currentText.length < resultTxt.length) {
@@ -15,17 +15,18 @@ function type() {
     }
 }
 
+// submit tweet link
 async function onSubmit() {
     inputEle.select();
     resultEle.innerHTML = '';
     tweetEle.innerHTML = '';
 
+    // get tweet from link
     let inputTxt = String(inputEle.value);
     tweetLoader.classList.add("display");
 
     let response = await fetch(`http://127.0.0.1:5000/get?link=${inputTxt}`);
 
-    
     let tweet;
     if (response.ok) {
         tweet = await response.text();
@@ -47,7 +48,8 @@ async function onSubmit() {
         tweetEle.innerHTML = tweet;
         return
     }
-    
+
+    // get result from tweet
     resultLoader.classList.add("display");
     response = await fetch(`http://127.0.0.1:5000/predict?tweet=${tweet}`);
 
@@ -57,10 +59,10 @@ async function onSubmit() {
         resultEle.style.color = '#f5f5f5';
         if (result === 'YES') {
             document.body.classList.add("disasters");
-            resultTxt = "Your tweet is about REAL disasters.";
+            resultTxt = "This tweet is about REAL disasters.";
         } else if (result === 'NO') {
             document.body.classList.remove("disasters");
-            resultTxt = "Your tweet is not about disasters.";
+            resultTxt = "This tweet is not about disasters.";
         } else {
             document.body.classList.remove("disasters");
             resultTxt = "Can not get result!";
@@ -69,14 +71,14 @@ async function onSubmit() {
         type();
     } else {
         result = 'Can not get result. Please try again!';
-        resultEle.style.color = '#fab6c9';
+        resultEle.style.color = '#d00000';
         resultLoader.classList.remove("display");
         resultEle.innerHTML = result;
     }
 }
 
+// add submit event when click and press enter key
 submitBtn.addEventListener('click', onSubmit)
-
 inputEle.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         onSubmit();
